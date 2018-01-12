4ad6ff9b154d3a633bc0e2662eb7ad1d7107a7ec
Adam Campbell <adam@hotmeteor.com>
2018-01-12 13:10:33 -0600

    Fix provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,14 +29,6 @@ class AppServiceProvider extends ServiceProvider
                 'access_token' => config('services.rollbar.key'),
                 'environment' => config('app.env'),
                 'root' => base_path(),
-                'person_fn' => function () {
-                    if (Auth::check()) {
-                        return [
-                            'id' => (string)Auth::user()->id,
-                            'email' => Auth::user()->email,
-                        ];
-                    }
-                },
             ], false, false, false);
         }
     }
